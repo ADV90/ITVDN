7eaeeb5bc9a3bcd5a56064ede033f6f5f1cd98ef
ADV90 <ahtursky@gmail.com>
2022-06-09 13:43:25 +0300

    HW_41 Update

--- HW_4/HW_41.py
+++ HW_4/HW_41.py
@@ -1,7 +1,7 @@
 a = 1
 b = 4
 x = 0
-while a < b:
+while a <= b:
     x += a
     a += 1
 
---
+++ HW_5/HW_51.py
@@ -0,0 +1,25 @@
+def hello(name='Dima'):
+    print('Hello, ', name, '!', sep='')
+
+hello('Test')
+hello()
+
+# def listgen(ame):
+#     for i in range(1, 3):
+#         nameask = str(input("Name list: "))
+#         ame.append(nameask)
+#     print(ame)
+#     return ame
+#
+# def simple_Hello():
+#     name = input('Enter your name: ')
+#     if name in ame:
+#         print('Hello,', name + '.')
+#     else:
+#         print("Hello, Dima")
+#
+#
+# if __name__ == '__main__':
+#     ame = []
+#     listgen(ame)
+#     simple_Hello()
