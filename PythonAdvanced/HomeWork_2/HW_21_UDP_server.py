94d79fea610a08501baaae795318a3ff906e7790
ADV90 <ahtursky@gmail.com>
2022-08-11 18:54:43 +0300

    HW Advanced 2 UDP client/server

---
+++ PythonAdvanced/HomeWork_2/HW_21_UDP_server.py
@@ -0,0 +1,30 @@
+"""Создайте UDP сервер, который ожидает сообщения о новых устройствах в сети. Он принимает сообщения определенного
+формата, в котором будет идентификатор устройства и печатает новые подключения в консоль. Создайте UDP клиента,
+который будет отправлять уникальный идентификатор устройства на сервер, уведомляя о своем присутствии.
+"""
+
+import socket
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('127.0.0.1', 8888))
+    print('UDP-server started')
+
+    while True:
+        try:
+            result = sock.recv(1024)
+        except KeyboardInterrupt:
+            break
+        else:
+            msg = result.decode('utf-8')
+            if msg.strip().lower() == "stop":
+                break
+            else:
+                print('Добавлено устройство:', msg)
+    print('UDP-server finished')
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
